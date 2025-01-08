bdab424696edea6835def88c465d1ab7d6deada3
GMttL <gaby.mititelu@gmail.com>
2025-01-08 11:52:07 +0000

    forgot to add gitignore

--- main.py
+++ main.py
@@ -7,7 +7,13 @@ def main():
 	print(f"Screen width: {constants.SCREEN_WIDTH}")
 	print(f"Screen height: {constants.SCREEN_HEIGHT}")
 
+	# setting up pygame
+	pygame.init()
+	screen = pygame.display.set_mode((constants.SCREEN_WIDTH, constants.SCREEN_HEIGHT))
 
+	while True:
+		screen.fill(color="black")
+		pygame.display.flip()
 
 if __name__ == "__main__":
 	main()
