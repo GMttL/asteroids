67c62c64c68201408fbf1799b0653946b9bc5b93
GMttL <gaby.mititelu@gmail.com>
2025-01-09 14:05:39 +0000

    added player

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import pygame
 import constants
-
+from player import Player
 
 def main():
 	print("Starting asteroids!")
@@ -10,10 +10,22 @@ def main():
 	# setting up pygame
 	pygame.init()
 	screen = pygame.display.set_mode((constants.SCREEN_WIDTH, constants.SCREEN_HEIGHT))
+	game_clock = pygame.time.Clock()
+	delta_time = 0
 
+	# Player
+	player = Player(constants.SCREEN_WIDTH / 2, constants.SCREEN_HEIGHT / 2)
+
+	# Game Loop
 	while True:
+		for event in pygame.event.get():
+			if event.type == pygame.QUIT:
+				return
 		screen.fill(color="black")
+		player.draw(screen)
 		pygame.display.flip()
+		delta_time = game_clock.tick(60) // 1000
+		
 
 if __name__ == "__main__":
 	main()
